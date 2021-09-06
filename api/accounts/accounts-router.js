71504411bde0155941aa5b97f00bb4d42f7be283
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-09-06 10:29:14 -0600

    esLint fix

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -60,8 +60,8 @@ router.delete('/:id', md.checkAccountId, async (req, res, next) => {
   }
 })
 
+// eslint-disable-next-line
 router.use((err, req, res, next) => {
-  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
   })
